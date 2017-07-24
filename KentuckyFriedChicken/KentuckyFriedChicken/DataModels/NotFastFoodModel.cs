4305e967e0bab30ea54cb67d73b3dbe6169bbee6
jc018 <jcha752@aucklanduni.ac.nz>
2017-07-24 19:59:54 +1200

    Revert "Changwe20"
    This reverts commit dff8ad9821c82c2d782b472e5a4031fb61d8c527.

--- KentuckyFriedChicken/KentuckyFriedChicken/DataModels/NotFastFoodModel.cs
+++ KentuckyFriedChicken/KentuckyFriedChicken/DataModels/NotFastFoodModel.cs
@@ -12,10 +12,10 @@ namespace KentuckyFriedChicken.DataModels
         [JsonProperty(PropertyName = "Id")]
         public string ID { get; set; }
 
-        [JsonProperty(PropertyName = "Type")]
-        public string Type { get; set; }
+        [JsonProperty(PropertyName = "Longitude")]
+        public float Longitude { get; set; }
 
-        [JsonProperty(PropertyName = "Quantity")]
-        public float Quantity { get; set; }
+        [JsonProperty(PropertyName = "Latitude")]
+        public float Latitude { get; set; }
     }
 }
