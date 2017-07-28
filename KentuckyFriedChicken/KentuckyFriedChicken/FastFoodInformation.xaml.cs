94c34f4dc1120b574d6db8cd0a4c490ea7052ce1
jc018 <jcha752@aucklanduni.ac.nz>
2017-07-28 23:35:08 +1200

    c178

--- KentuckyFriedChicken/KentuckyFriedChicken/FastFoodInformation.xaml.cs
+++ KentuckyFriedChicken/KentuckyFriedChicken/FastFoodInformation.xaml.cs
@@ -17,7 +17,22 @@ namespace KentuckyFriedChicken
 
             InitializeComponent();
         }
+        void click1(object sender, EventArgs args)
+        {
+            Device.OpenUri(new Uri("tel:092713540"));
+        }
+        void click2(object sender, EventArgs args)
+        {   
+            Device.OpenUri(new Uri("tel:095762013"));
+        }
+        void click3(object sender, EventArgs args)
+        {
+            Device.OpenUri(new Uri("tel:095282145"));
+        }
+        void click4(object sender, EventArgs args)
+        {
+            Device.OpenUri(new Uri("tel:092727715"));
+        }
 
-  
     }
 }
